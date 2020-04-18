a773f67334330f6cca3a7934527febfbe62e536c
Christen <shub0014@algonquinlive.com>
2020-04-17 20:23:19 -0400

    finished moblie and most of that infernal hamburger nav thats 18 hours i'll never get back

---
+++ shorthand.py
@@ -0,0 +1,14 @@
+import time
+
+
+#varibles
+t = input("Top: " )
+r = input("Right: " )
+b = input("Bottom: " )
+l = input("Left: " )
+
+#printing
+print("margin:" + t + "rem " + r + "rem " + b + "rem " + l +"rem;")
+#delay
+time.sleep(5)
+print ("ty")
